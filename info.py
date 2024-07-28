eed647b1f6cb319eb66480cf89475faf5daecdca
superpasses <165167006+superpasses@users.noreply.github.com>
2024-07-28 22:44:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Its_me_reyan
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@bilijacksan?si=zmzfpCATZFGk77Fj
+# Ask Doubt on telegram @its_me_reyana
 
 # Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
 
@@ -9,7 +9,7 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "29726755"))
 API_HASH     = environ.get("API_HASH", "0a708211226ff747f38c47a80a5e29c0")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "6517100010:AAHEzkYD9fGW-btrnkY16vPiAgT1tSv3lIE")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "6826129519:AAERNPqV9nuoW9wuFAVbxvtjG0uXIFfbW8g")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://deepchutia95:tmpGRbjdJE1WTpG8@cluster0.wrwzavb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002167800454"))
 ADMIN        = int(environ.get("ADMIN", "7481882166"))
