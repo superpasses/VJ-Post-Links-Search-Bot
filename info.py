cbf4fe74f635e25b3173d479dd3788fac329bfad
superpasses <165167006+superpasses@users.noreply.github.com>
2024-07-28 16:44:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "29726755"))
 API_HASH     = environ.get("API_HASH", "0a708211226ff747f38c47a80a5e29c0")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "6517100010:AAHEzkYD9fGW-btrnkY16vPiAgT1tSv3lIE")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://deepchutia95:tmpGRbjdJE1WTpG8@cluster0.wrwzavb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002167800454"))
 ADMIN        = int(environ.get("ADMIN", "7481882166"))
 CHANNEL      = environ.get("CHANNEL", "@MKD_Korean_Drama")
